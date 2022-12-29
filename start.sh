c7216b90158ede527419556b72efbca129acfecd
DEMIGOD-AKSHAY <91306144+DEMIGOD-AKSHAY@users.noreply.github.com>
2022-12-29 08:02:00 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/DEMIGOD-AKSHAY/MdiskSearchBotV2.git /MdiskSearchBotV1
+cd /MdiskSearchBotV1
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
